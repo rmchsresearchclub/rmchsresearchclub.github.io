baaba2f3f73baf25e1d32e2efc0cec62474676ca
brickwall2900 <brickwall2900@gmail.com>
2026-01-22 15:41:48 +0800

    create new ImageViewer component

---
+++ src/lib/components/ImageViewer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { Viewer } from "svelte-image-viewer";
+
+    let { src, alt, closePopup, ...others } = $props();
+
+</script>
+
+<dialog class="fixed w-full h-full mx-auto my-auto inset-0 bg-black/70 flex items-center justify-center z-50">
+    <div class="relative w-full h-full flex items-center justify-center p-2">
+        <p class="absolute w-full top-1 text-center text-white mix-blend-exclusion p-2 z-20">You're in an image viewer. Drag to pan the image around, pinch or mouse wheel to scroll.</p>
+        <Viewer>
+            <enhanced:img src={src} alt={alt} class="rounded shadow-lg max-w-full max-h-full z-10" {...others} />
+        </Viewer>
+        <button class="absolute top-2 right-2 m-2 text-white bg-black border border-white opacity-60 rounded-full px-4 py-3 hover:opacity-70 z-30" onclick={closePopup} >
+            Close
+        </button>
+    </div>
+</dialog>
