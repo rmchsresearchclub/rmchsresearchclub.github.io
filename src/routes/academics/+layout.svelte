70e8d779d69925c69dbd2fce745689bd0b503103
brickwall2900 <brickwall2900@gmail.com>
2026-01-23 09:32:42 +0800

    navigation button + mobile responsiveness

---
+++ src/routes/academics/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import type { LayoutProps } from './$types';
+    import { asset, resolve } from "$app/paths";
+	import Icon from '@iconify/svelte';
+
+    let { data, children }: LayoutProps = $props();
+</script>
+
+<header class="fixed top-0 left-0 w-full">
+    <nav class="w-full pr-2">
+        <div class="m-4 flex flex-row gap-4">
+            <!-- tailwind classes are larger than the fricking mount everest -->
+            <a class="rounded-full border border-black backdrop-blur-xs px-2 py-1 flex flex-row items-center gap-2 text-sm text-center lg:text-md lg:text-left" 
+                href={resolve("/academics")}>
+                <Icon icon="ep:back" class="inline size-8 lg:size-4" />
+                <span class="hidden lg:inline">Back to RMCHS Website</span>
+            </a>
+            <a class="rounded-full border border-black backdrop-blur-xs px-2 py-1 flex flex-row items-center gap-2 text-sm text-center lg:text-md lg:text-left" 
+                href={resolve("/academics")}>
+                <Icon icon="charm:graduate-cap" class="inline size-8 lg:size-4" />
+                <span class="hidden lg:inline">List of Academics</span>
+            </a>
+        </div>
+    </nav>
+</header>
+
+{@render children()}
--- src/routes/academics/+page.svelte
+++ src/routes/academics/+page.svelte
@@ -4,23 +4,21 @@
     import { asset, resolve } from "$app/paths";
 
     let { data }: PageProps = $props();
-
-
 </script>
 
 <h1 class="text-3xl font-bold w-full text-center underline underline-offset-12 my-12">Academics</h1>
 
 {#snippet card(icon: string, name: string, sublink: string, otherClass: string)}
-    <article class={"border p-4 flex flex-row items-center justify-between gap-2 " + otherClass}>
-        <Icon icon={icon} height={80} />
+    <article class={"border p-4 flex flex-col lg:flex-row items-center justify-between gap-2 " + otherClass}>
+        <Icon class="w-20" icon={icon} height={80} />
         <h1 class="font-bold text-xl">{name}</h1>
-        <a href={resolve(`/academics/${sublink}`)} class="border p-2 border-black hover:bg-gray-300 no-underline text-black">Learn more</a>
+        <a href={resolve(`/academics/${sublink}`)} class="border p-2 border-black hover:bg-gray-300 motion-safe:transition motion-safe:duration-150 no-underline text-black">Learn more</a>
     </article>
 {/snippet}
 
 <section class="max-w-10/12 mx-auto flex flex-col gap-4">
     {@render card("mdi:teach", "Basic Education Curriculum", "bec", "")}
-    {@render card("icomoon-free:lab", "Science, Technology, and Engineering Program", "ste", "")}
+    {@render card("material-symbols:science", "Science, Technology, and Engineering Program", "ste", "")}
     {@render card("mdi:paint-outline", "Special Program in the Arts", "spa", "")}
     {@render card("charm:graduate-cap", "Senior High School", "shs", "")}
     {@render card("mi:document", "Alternative Learning System", "als", "")}
