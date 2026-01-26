0c74d6a53e1af2f99c2327d6002490d134858357
brickwall2900 <brickwall2900@gmail.com>
2026-01-27 00:53:31 +0800

    add spoiler component

---
+++ src/lib/components/Spoiler.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    let { children } = $props();
+    let spoilerShown = $state(false);
+
+    function onSpoilerClicked(e: Event) {
+        spoilerShown = true;
+        e.stopPropagation();
+    }
+</script>
+
+{#if !spoilerShown}
+    <button 
+        class="blur-[6px] hover:blur-xs text-left" 
+        title="View spoiler"
+        onclick={onSpoilerClicked}>
+        {@render children?.()}
+    </button>
+{:else}
+    {@render children?.()}
+{/if}
